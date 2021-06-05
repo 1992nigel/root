ade0698bafbd3bab3e9e7673a1825a4b2e55b72d
Nigel, (nownigel.com) <nownigel@gmail.com>
2021-06-04 21:11:34 -0400

    [nj]000

--- wordpress/phlygh_wp/footer.php
+++ wordpress/phlygh_wp/footer.php
@@ -6,13 +6,13 @@
 		</div>
 
 		<div class="links bg_black">
-			<a class="button_g" href="https://phlygh.org">phlygh:</a>
 			<a class="button_h" href="https://phlygh.org/about/">about</a>
 			<a class="button_h" href="https://phlygh.org/staff/">staff</a>
 			<a class="button_h" href="https://phlygh.org/news/">news</a>
 			<a class="button_h" href="http://phlygh.org/category/shop/">store</a>
 			<a class="button_h" href="http://phlygh.org/category/locations/">locations</a>
 			<a class="button_h" href="https://phlygh.org/support/">support</a>
+			<a class="button_g" href="https://phlygh.org">phlygh:</a>
 		</div>
 
 
